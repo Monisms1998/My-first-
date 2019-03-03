582659cb24bcbb0767cc06ac03551b10542fd94e
Monisms1998 <asjad.siddiqui2001@gmail.com>
2019-03-03 07:06:21 -0800

    my-commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// var name=document.CheckingValidation.name.value;
+// var pasword=document.CheckingValidation.pasword.value;
+// if(name==""||name==null)
+// {
+//     alert("A name could not be null vlue or emoty string");
+//     return false;
+// }
+// else if(pasword.length<6)
+// {
+//     alert("Too short pasword");
+//     return false
+let a=document.getElementsByClassName("row");
+addEventListener('click',function(e){
+    a.style.backgroundcolor="black";
+})
+
